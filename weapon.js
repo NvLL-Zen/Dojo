5ee18766bc5579a377324a16be49d4904dde4fcc
Zen <fauzanrizky13@smaislamsinarecendekia.sch.id>
2023-10-31 10:49:54 +0700

    Added "Personalities"

--- weapon.js
+++ weapon.js
@@ -5,8 +5,13 @@ class Weapon {
         this.name = name;
         this.category = category;
         this.nationality = nationality
-        this.shoot = () => {
-            console.log("PEW! PEW! BLYAAAT!");
+        this.shoot = (state="Normal") => {
+            if (state == "Normal"){
+                console.log("PEW! PEW! BLYAAAT!");
+            }
+            else if (state == "Headshot") { 
+                console.log("BANG! HEADSHOT CYKAA!")
+            }
         };
     }
 }
@@ -17,3 +22,4 @@ console.log(AK47.name)
 console.log(AK47.category)
 console.log(AK47.nationality)
 AK47.shoot()
+AK47.shoot("Headshot")
